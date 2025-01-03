95cb87c09e34a3f3d67615b3b2e85eb122ed0c10
Håkon Anders Strømsodd <haakon8855@gmail.com>
2025-01-03 10:49:29 +0100

    Refactor timezone conversion

--- src/Web/DateTimeExtensions.cs
+++ src/Web/DateTimeExtensions.cs
@@ -2,11 +2,10 @@
 
 public static class DateTimeExtensions
 {
-	public static string ToLocalDateString(this DateTime utcTime)
-	{
-		var timezone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-		var localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, timezone);
-
-		return $"{localTime.ToShortDateString()} {localTime.ToShortTimeString()}";
-	}
+    public static string ToLocalDateTimeString(this DateTime utcTime)
+    {
+        var timezone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+        var localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, timezone);
+        return $"{localTime.ToShortDateString()} {localTime.ToShortTimeString()}";
+    }
 }
