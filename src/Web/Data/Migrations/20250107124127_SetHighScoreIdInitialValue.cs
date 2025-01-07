9f259a7ce5ebb663df89022162d03e53f702eb45
Håkon <haakon8855@gmail.com>
2025-01-07 13:41:48 +0100

    Set HighScoreId initial value

---
+++ src/Web/Data/Migrations/20250107124127_SetHighScoreIdInitialValue.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetHighScoreIdInitialValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE AspNetUsers SET AspNetUsers.HighScoreId = (
+                    SELECT TOP 1 Scores.Id
+                    FROM Scores
+                    WHERE Scores.UserId = AspNetUsers.Id
+                    ORDER BY Scores.Points DESC
+                )
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
