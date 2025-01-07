8228b1f9eb018e5e972689a887d1d824f9904347
Håkon <haakon8855@gmail.com>
2025-01-07 14:01:42 +0100

    Set statis initial values

---
+++ src/Web/Data/Migrations/20250107130123_SetStatsInitialValues.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetStatsInitialValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE AspNetUsers SET AspNetUsers.StatsGamesStarted = (
+                    SELECT Count(*)
+                    FROM Scores
+                    WHERE Scores.UserId = AspNetUsers.Id
+                )               
+            ");
+
+            migrationBuilder.Sql(@"
+                UPDATE AspNetUsers SET AspNetUsers.StatsTotalPoints = (
+                    SELECT Sum(Points)
+                    FROM Scores
+                    WHERE Scores.UserId = AspNetUsers.Id
+                )               
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
